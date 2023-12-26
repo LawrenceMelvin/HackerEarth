473ee3533c56d4b7c3c7fbefb454fee4155c547f
lawrence <lawrence2000april@gmail.com>
2023-12-26 22:23:19 +0530

    solution for hackerearth

---
+++ Best_Index/index.java
@@ -0,0 +1,20 @@
+package Best_Index;
+
+import java.util.Scanner;
+
+public class index {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int a[] = new int[N];
+        for(int i =0;i<N;i++){
+            a[i] = sc.nextInt();
+        }
+        /*
+        for(int j=0;j<a.length;j++){
+            System.out.print(a[j]+" ");
+        }
+         */
+        
+    }
+}
